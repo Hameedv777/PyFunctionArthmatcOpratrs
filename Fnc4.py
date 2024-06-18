f40dec46390073f924b45513419ce2b76b5383ef
Abdul Hameed V <160454030+Hameedv777@users.noreply.github.com>
2024-06-18 11:26:00 +0530

    Add files via upload

---
+++ Fnc4.py
@@ -0,0 +1,32 @@
+num1= int(input("Enter the first number:-"))
+num2= int(input("Enter the Second number number:-"))
+print("===Function to print sum===")
+def add(a,b):
+    a=num1
+    b=num2
+    result=num1+num2
+    return result
+print(add(num1,num2))
+
+print("===Function to prin Diffrent===")
+def sub(a,b):
+    a=num1
+    b=num2
+    result=num1-num2
+    return result
+print(sub(num1,num2))
+print("===Function to print Multiplication===")
+def mlty(a,b):
+    a=num1
+    b=num2
+    result=num1*num2
+    return result
+print(mlty(num1,num2))
+print("===Function to print Division===")
+def dvsn(a,b):
+    a=num1
+    b=num2
+    result=num1/num2
+    return result
+print(dvsn(num1,num2))
+
